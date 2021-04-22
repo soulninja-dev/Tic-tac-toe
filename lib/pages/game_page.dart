63818e7dfa414123a76f12c99c872757ae0fa2a8
SoulNinja-dev <aadhi123awesome@gmail.com>
2021-04-22 20:28:19 +0530

    👨‍💻 logic built

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -7,7 +7,8 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
-  changeToPlay(String current) {
+  // ignore: non_constant_identifier_names
+  change_toPlay(String current) {
     if (current == "X") {
       setState(() {
         toPlay = "Y";
@@ -20,6 +21,18 @@ class _GamePageState extends State<GamePage> {
     }
   }
 
+/*
+  Boolean played array -> to store which buttons are clicked
+  
+  function play -> to take in index and change text on it to whatever toPlay is
+                      -> should change played array also
+
+  function botPlay -> random from 0-8 and check if the box is taken in played array and call play
+
+  function change_toPlay -> change the text in toPlay
+
+  String toPlay -> variable X or Y to show in the bottom part 
+*/
   static String toPlay = "X";
   @override
   Widget build(BuildContext context) {
@@ -35,39 +48,39 @@ class _GamePageState extends State<GamePage> {
               crossAxisCount: 3,
               children: <Widget>[
                 TextButton(
-                  onPressed: () => changeToPlay(toPlay),
+                  onPressed: () => change_toPlay(toPlay),
                   child: Text("here1"),
                 ),
                 TextButton(
-                  onPressed: () => changeToPlay(toPlay),
+                  onPressed: () => change_toPlay(toPlay),
                   child: Text("here2"),
                 ),
                 TextButton(
-                  onPressed: () => changeToPlay(toPlay),
+                  onPressed: () => change_toPlay(toPlay),
                   child: Text("here3"),
                 ),
                 TextButton(
-                  onPressed: () => changeToPlay(toPlay),
+                  onPressed: () => change_toPlay(toPlay),
                   child: Text("here4"),
                 ),
                 TextButton(
-                  onPressed: () => changeToPlay(toPlay),
+                  onPressed: () => change_toPlay(toPlay),
                   child: Text("here5"),
                 ),
                 TextButton(
-                  onPressed: () => changeToPlay(toPlay),
+                  onPressed: () => change_toPlay(toPlay),
                   child: Text("here6"),
                 ),
                 TextButton(
-                  onPressed: () => changeToPlay(toPlay),
+                  onPressed: () => change_toPlay(toPlay),
                   child: Text("here7"),
                 ),
                 TextButton(
-                  onPressed: () => changeToPlay(toPlay),
+                  onPressed: () => change_toPlay(toPlay),
                   child: Text("here8"),
                 ),
                 TextButton(
-                  onPressed: () => changeToPlay(toPlay),
+                  onPressed: () => change_toPlay(toPlay),
                   child: Text("here9"),
                 ),
               ],
