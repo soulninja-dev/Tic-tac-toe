fef5f19863bc871587f5a3121dd2dcd1f8a59cb6
SoulNinja-dev <aadhi123awesome@gmail.com>
2021-05-04 15:14:36 +0530

    that was the most silliest bug 🤦 logic fixed 😅

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -84,35 +84,35 @@ Possibilities to win:
   // this will check if the game is finished or not
   bool winCheck() {
     // 012
-    if (text[0] == text[1] && text[1] == text[2]) {
+    if (text[0] != "" && text[0] == text[1] && text[1] == text[2]) {
       return true;
     }
     // 345
-    else if (text[3] == text[4] && text[4] == text[5]) {
+    else if (text[3] != "" && text[3] == text[4] && text[4] == text[5]) {
       return true;
     }
     // 678
-    else if (text[6] == text[7] && text[7] == text[8]) {
+    else if (text[6] != "" && text[6] == text[7] && text[7] == text[8]) {
       return true;
     }
     // 036
-    else if (text[0] == text[3] && text[3] == text[6]) {
+    else if (text[0] != "" && text[0] == text[3] && text[3] == text[6]) {
       return true;
     }
     // 147
-    else if (text[1] == text[4] && text[4] == text[7]) {
+    else if (text[1] != "" && text[1] == text[4] && text[4] == text[7]) {
       return true;
     }
     // 258
-    else if (text[2] == text[5] && text[5] == text[8]) {
+    else if (text[2] != "" && text[2] == text[5] && text[5] == text[8]) {
       return true;
     }
     // 048
-    else if (text[0] == text[4] && text[4] == text[8]) {
+    else if (text[0] != "" && text[0] == text[4] && text[4] == text[8]) {
       return true;
     }
     // 246
-    else if (text[2] == text[4] && text[4] == text[6]) {
+    else if (text[2] != "" && text[2] == text[4] && text[4] == text[6]) {
       return true;
     }
     return false;
