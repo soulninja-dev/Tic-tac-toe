f53b43dd896906ca01d79873ed2dfe591615886b
SoulNinja-dev <aadhi123awesome@gmail.com>
2021-05-05 17:33:51 +0530

    ➕ added reset function in Functions class

---
+++ lib/functions.dart
@@ -0,0 +1,17 @@
+import 'package:tictactor/pages/game_page.dart';
+
+class Functions {
+  static void reset() {
+    text = [
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+    ];
+  }
+}
