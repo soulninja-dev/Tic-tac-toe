7bacd235c0539948a81c3f2b037fddb872b2d7eb
SoulNinja-dev <aadhi123awesome@gmail.com>
2021-05-07 17:47:16 +0530

    added more constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 
 Color bg = new Color(0xff18363E);
-Color textColor = Colors.white;
+Color white = Colors.white;
+Color black = Colors.black;
+Color btnbg = Color(0xff2D5F6E);
+BorderRadius br = const BorderRadius.all(Radius.circular(25.0));
