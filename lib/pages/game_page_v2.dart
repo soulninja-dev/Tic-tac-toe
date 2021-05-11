6191931c7bcf90744e41e04d2f61557ee60d8515
SoulNinja-dev <aadhi123awesome@gmail.com>
2021-05-11 20:01:49 +0530

    whoIsWinner function

--- lib/pages/game_page_v2.dart
+++ lib/pages/game_page_v2.dart
@@ -28,6 +28,7 @@ class _GamePagev2State extends State<GamePagev2> {
                 - if board is empty, returns 0.
                 - else if any possibility of win (verticals | horizontals | diagonals) = "X", return 1.
                 - else if any possibility of win (verticals | horizontals | diagonals) = "O", return 2.
+                - 012, 036, 147, 258, 345, 678, 048, 246
                 - else if board is full ( tie breaker ), return 3
                 - else return 0
             - call winCheck with the variable saved.
@@ -68,6 +69,21 @@ class _GamePagev2State extends State<GamePagev2> {
 
   // FUNCTIONS
 
+  bool existsHuman(List<String> arr)
+  {
+    if(humanPlays.contains(arr[0]) && humanPlays.contains(arr[1]) &&
+        humanPlays.contains(arr[2]))
+      return true;
+    return false;
+  }
+
+  bool existsBot(List<String> arr)
+  {
+    if(botPlays.contains(arr[0]) && botPlays.contains(arr[1]) &&
+        botPlays.contains(arr[2]))
+      return true;
+    return false;
+  }
   void clicked (int index) async {
 
     // set state of the board
@@ -78,11 +94,23 @@ class _GamePagev2State extends State<GamePagev2> {
       print(board);
     });
     int result = await whoIsWinner();
+    print("result: $result");
   }
 
   whoIsWinner() async{
+      // board is empty
       if(!board.contains("X") && !board.contains("O")) return 0;
-      else return 1;
+      // human win check
+      else if(existsHuman(["0","1","2"]) || existsHuman(["0","3","6"]) ||existsHuman(["7","4","1"]) ||existsHuman(["8","5","2"]) ||existsHuman(["3","4","5"]) ||existsHuman(["6","7","8"]) ||existsHuman(["0","4","8"]) ||existsHuman(["2","4","6"]))
+        return 1;
+      // bot win check
+      else if(existsBot(["0","1","2"]) || existsBot(["0","3","6"]) ||existsBot(["7","4","1"]) ||existsBot(["8","5","2"]) ||existsBot(["3","4","5"]) ||existsBot(["6","7","8"]) ||existsBot(["0","4","8"]) ||existsBot(["2","4","6"]))
+        return 2;
+      // tie check
+      else if(!board.contains(""))
+        return 3;
+      // no one has won yet, game will continue
+      else return 0;
   }
 
   Future<void> winCheck(int result) async{
